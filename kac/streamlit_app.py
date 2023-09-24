a98e2eaf4a99a1ed34f51fe3bb37078fad69dbb1
masigara <masigara@yahoo-corp.jp>
2023-09-24 15:07:38 +0900

    update

--- kac/streamlit_app.py
+++ kac/streamlit_app.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-date = "20230923"
+date = "20230924"
+rounds = ["a", "b"]
 titles = ["2dx", "sdvx", "popn", "ddr"]
 
 st.set_page_config(
@@ -12,12 +13,15 @@ st.set_page_config(
 
 for i, t in enumerate(st.tabs(titles)):
     with t:
-        st.write(f"KAC 2023 {titles[i]} 予選ラウンドB")
-        st.write(f"最終更新 {date}")
-        df = pd.read_csv(f"{titles[i]}_{date}.csv")
-        df.index = np.arange(1, len(df) + 1)
-        st.dataframe(df)
+        for j, r in enumerate(st.tabs(rounds)):
+            if not (i == 2 and j == 0):  # popn a
+                with r:
+                    st.write(f"KAC 2023 {titles[i]} 予選ラウンド{rounds[j].upper()}")
+                    st.write(f"最終更新 {date}")
+                    df = pd.read_csv(f"csv/{titles[i]}_{rounds[j]}_{date}.csv")
+                    df.index = np.arange(1, len(df) + 1)
+                    st.dataframe(df)
 
-        st.bar_chart(
-            df.iloc[1:, 2:],
-        )
+                    st.bar_chart(
+                        df.iloc[1:, 2:],
+                    )
